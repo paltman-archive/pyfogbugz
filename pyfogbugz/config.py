89d2147b828b818a5f0cb1b0952765c1b3bc4b0f
Patrick Altman <patrick@studionow.com>
2009-01-10 18:04:06 -0600

    Implemented basic connection object and added some custom exceptions.
    - Validates API
    - Authenticates session
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   README
    #	modified:   pyfogbugz/__init__.py
    #	new file:   pyfogbugz/config.py
    #	modified:   pyfogbugz/connection.py
    #	modified:   pyfogbugz/exceptions.py
    #	modified:   pyfogbugz/workingschedule.py
    #
    # Untracked files:
    #   (use "git add <file>..." to include in what will be committed)
    #
    #	test.py

---
+++ pyfogbugz/config.py
@@ -0,0 +1,108 @@
+# Copyright (c) 2009 Patrick Altman http://paltman.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+# Most of this file was borrowed from boto
+
+import StringIO, os
+import ConfigParser
+
+ConfigLocations = ['/etc/pyfogbugz.cfg', os.path.expanduser('~/.pyfogbugz')]
+ConfigPath = ConfigLocations[0]
+UserConfigPath = ConfigLocations[1]
+
+class Config(ConfigParser.SafeConfigParser):
+
+    def __init__(self, path=None, fp=None):
+        ConfigParser.SafeConfigParser.__init__(self, {'working_dir' : '/tmp',
+                                                      'debug' : '0'})
+        if path:
+            self.read(path)
+        elif fp:
+            self.readfp(fp)
+        else:
+            self.read(ConfigLocations)
+
+    def save_option(self, path, section, option, value):
+        """
+        Write the specified Section.Option to the config file specified by path.
+        Replace any previous value.  If the path doesn't exist, create it.
+        Also add the option the the in-memory config.
+        """
+        config = ConfigParser.SafeConfigParser()
+        config.read(path)
+        if not config.has_section(section):
+            config.add_section(section)
+        config.set(section, option, value)
+        fp = open(path, 'w')
+        config.write(fp)
+        fp.close()
+        if not self.has_section(section):
+            self.add_section(section)
+        self.set(section, option, value)
+
+    def save_user_option(self, section, option, value):
+        self.save_option(os.path.expanduser(UserConfigPath), section, option, value)
+
+    def save_system_option(self, section, option, value):
+        self.save_option(ConfigPath, section, option, value)
+
+    def get_value(self, section, name, default=None):
+        return self.get(section, name, default)
+
+    def get(self, section, name, default=None):
+        try:
+            val = ConfigParser.SafeConfigParser.get(self, section, name)
+        except:
+            val = default
+        return val
+    
+    def getint(self, section, name, default=0):
+        try:
+            val = ConfigParser.SafeConfigParser.getint(self, section, name)
+        except:
+            val = int(default)
+        return val
+    
+    def getfloat(self, section, name, default=0.0):
+        try:
+            val = ConfigParser.SafeConfigParser.getfloat(self, section, name)
+        except:
+            val = float(default)
+        return val
+
+    def getbool(self, section, name, default=False):
+        if self.has_option(section, name):
+            val = self.get(section, name)
+            if val.lower() == 'true':
+                val = True
+            else:
+                val = False
+        else:
+            val = default
+        return val
+    
+    def setbool(self, section, name, value):
+        if value:
+            self.set(section, name, 'true')
+        else:
+            self.set(section, name, 'false')
+    
+
---
+++ pyfogbugz/connection.py
@@ -0,0 +1,177 @@
+# Copyright (c) 2009 Patrick Altman http://paltman.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+"""
+Provides connectivity to FogBugz
+"""
+import urllib2
+import urlparse
+import socket
+import errno
+import xml.sax
+from xml.sax.handler import ContentHandler
+
+from pyfogbugz import config, UserAgent
+from pyfogbugz.exceptions import FogBugzClientError, FogBugzServerError
+
+
+class Connection(object):
+    def __init__(self, url=None, username=None, password=None):
+        if url:
+            self.url = url
+        elif config.has_option('Connection', 'url'):
+            self.url = config.get('Connection', 'url')
+        
+        if username:
+            self.username = username
+        elif config.has_option('Connection', 'username'):
+            self.username = config.get('Connection', 'username')
+        
+        if password:
+            self.password = password
+        elif config.has_option('Connection', 'password'):
+            self.password = config.get('Connection', 'password')
+        
+    def make_request(self, path, data=None):
+        response = None
+        headers = {'User-Agent':UserAgent}
+        if data:
+            headers['Content-Length'] = len(data)
+        try:
+            url = "%s/%s" % (self.url, path)
+            request = urllib2.Request(url=url, headers=headers)
+            response = urllib2.urlopen(request)
+            if response.code < 300:
+                return response
+        except KeyboardInterrupt:
+            sys.exit("Keyboard Interrupt")
+        except Exception, e:
+            pass
+        
+        if response:
+            raise FogBugzServerError(response.code, response.msg, response.read())
+        elif e:
+            raise e
+        else:
+            raise FogBugzClientError("Please report this exception as an issue with pyfogbugz.")
+            
+
+class XmlHandler(object, ContentHandler):
+    def __init__(self):
+        self.error_code = None
+        self.error_message = None
+        self.has_error = False
+        self.current_value = ''
+    def characters(self, content):
+        self.current_value += content
+    def startElement(self, name, attrs):
+        self.current_value = ''
+        if name == 'error':
+            self.error_code = attr['code']
+            self.has_error = True
+    def endElement(self, name):
+        if name == 'error':
+            self.error_message = self.current_value
+        self.current_value = ''
+        
+        
+class ApiCheckHandler(XmlHandler):
+    def __init__(self):
+        super(ApiCheckHandler, self).__init__()
+        self.version = ''
+        self.minversion = ''
+        self.url = ''
+        
+    def endElement(self, name):
+        if name == 'version':
+            self.version = self.current_value
+        elif name == 'minversion':
+            self.minversion = self.current_value
+        elif name == 'url':
+            self.url = self.current_value
+        super(ApiCheckHandler, self).endElement(name)
+        
+
+class LogonHandler(XmlHandler):
+    def __init__(self):
+        super(LogonHandler, self).__init__()
+        self.token = None
+        self.people = None
+    
+    def startElement(self, name, attrs):
+        super(LogonHandler, self).startElement(name, attrs)
+        if name == 'people':
+            self.people = []
+    
+    def endElement(self, name):
+        if name == 'token':
+            self.token = self.current_value
+        elif name == 'person':
+            self.people.append(self.current_value)
+        super(LogonHandler, self).endElement(name)
+    
+        
+    
+class FogBugzConnection(Connection):
+    def __init__(self, url=None, username=None, password=None):
+        """
+        @type url: string
+        @param url: The url to the FogBugz server to connect to
+        
+        @type username: string
+        @param username: The username for the connection
+        
+        @type password: string
+        @param password: The password for the connection
+        """
+        self.base_path = None
+        super(FogBugzConnection, self).__init__(url, username, password)
+        self._check_api()
+        self._logon()
+        
+        
+    def make_request(self, path, data=None):
+        computed_path = path
+        if self.base_path:
+            computed_path = "%s%s" % (self.base_path, path)
+        return super(FogBugzConnection, self).make_request(computed_path, data)
+    
+    def _check_api(self):
+        response = self.make_request('api.xml')
+        if response:
+            data = response.read()
+            handler = ApiCheckHandler()
+            xml.sax.parseString(data, handler)
+            self.base_path = handler.url
+        else:
+            raise FogBugzClientError("Could not validate API.")
+    
+    def _logon(self):
+        response = self.make_request('cmd=logon&email=%s&password=%s' % (self.username, self.password))
+        if response:
+            data = response.read()
+            handler = LogonHandler()
+            xml.sax.parseString(data, handler)
+            if handler.has_error:
+                raise FogBugzClientError("Invalid login: %s" % handler.error_message)
+            else:
+                self.token = handler.token
+    
